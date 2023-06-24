bf15edcbc298ff037bbb674281e228793c67b239
Swaraa1211 <saiswaroopasaranath1211@gmail.com>
2023-06-24 15:57:49 +0530

    (add): Typewritter style

--- ArtVistaClient/src/pages/Auth/Login.js
+++ ArtVistaClient/src/pages/Auth/Login.js
@@ -14,6 +14,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import { Login } from '../../API/users';
 import { useContext } from 'react';
 import { AuthContext } from './authProvider';
+import TypewriterEffect from '../../utils/typeWritter';
 
 export default function LoginPage() {
   const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
@@ -44,34 +45,44 @@ export default function LoginPage() {
   }
   return (
     <>
-      <Flex minHeight="100vh" width="full" align="center" justifyContent="center">
+      <Flex
+        minHeight="100vh"
+        width="full"
+        align="center"
+        justifyContent="center"
+        bg="url('https://i.pinimg.com/564x/0b/6a/45/0b6a45aeeaf2de806e15f6a3c2f5d555.jpg')"
+        bgPosition="center"
+        bgRepeat="no-repeat"
+        bgSize="cover"
+
+      >
         <Box
           p={6}
           maxWidth="450px"
           maxHeight="900px"
           width="full"
-          bg="url('https://images.unsplash.com/photo-1543857778-c4a1a3e0b2eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGFydHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60')"
+          // bg="url('https://images.unsplash.com/photo-1543857778-c4a1a3e0b2eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGFydHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60')"
           // bgImage="url('src/assets/images/Untitled.jpeg')"
-          bgSize="cover"
+          // bgSize="cover"
           bgPosition="center"
           position="relative"
-          top={0}
-          left={0}
-          right={0}
-          bottom={0}
-          zIndex={-1}
+        // top={0}
+        // left={0}
+        // right={0}
+        // bottom={0}
+        // zIndex={-1}
         >
           <Box textAlign="center">
-            <Heading color="white">Login</Heading>
+            <Heading color="black">ᒪOGIᑎ</Heading>
           </Box>
           <Box p={4}>
             <form onSubmit={handleSubmit}>
               <FormControl isRequired>
-                <FormLabel color="white">Email</FormLabel>
+                <FormLabel color="black">Email</FormLabel>
                 <Input type="email" name="email" placeholder="test@gmail.com" />
               </FormControl>
               <FormControl mt={2} isRequired>
-                <FormLabel color="white">Password</FormLabel>
+                <FormLabel color="black">Password</FormLabel>
                 <Input type="password" name="password" placeholder="******" />
               </FormControl>
               <Button mt={4} type="submit" width="full" colorScheme="blue">
@@ -87,8 +98,18 @@ export default function LoginPage() {
             </Text>
           </Box>
         </Box>
-        <Box>
-          <Text textAlign="center">Some additional text or information here</Text>
+        <Box
+          p={6}
+          maxWidth="450px"
+          maxHeight="900px"
+          width="full"
+          align="center"
+        justifyContent="center"
+        >
+          <Text fontSize="3xl" fontWeight="bold">ᗯEᒪᑕOᗰE TO ᗩᖇT ᐯIᔕTᗩ!!! </Text>
+          <TypewriterEffect text="
+Art Vista is a vibrant and immersive platform that celebrates art in all its forms, serving as a go-to destination for artists looking to showcase their portfolios and art enthusiasts seeking inspiration." />
+          {/* <Text textAlign="center">Some additional text or information here</Text> */}
         </Box>
       </Flex>
 
---
+++ ArtVistaClient/src/utils/typeWritter.js
@@ -0,0 +1,22 @@
+import { Box } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
+
+const TypewriterEffect = ({ text }) => {
+  const [displayText, setDisplayText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (currentIndex < text.length) {
+        setDisplayText((prevText) => prevText + text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }
+    }, 100);
+
+    return () => clearTimeout(timer);
+  }, [currentIndex, text]);
+
+  return <Box as="span" fontSize="2xl" fontWeight="bold">{displayText}</Box>;
+};
+
+export default TypewriterEffect;
