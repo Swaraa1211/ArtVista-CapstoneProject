4679ea4589c782ac4de8cbd2c2e8305ec2d6ea6d
Swaraa1211 <saiswaroopasaranath1211@gmail.com>
2023-06-30 14:55:25 +0530

    (update): API fetch in Portfolio

--- ArtVistaClient/src/pages/profile/portfolioPage.js
+++ ArtVistaClient/src/pages/profile/portfolioPage.js
@@ -5,92 +5,58 @@ import { useRecoilValue } from 'recoil';
 import { userAtom } from '../../constant/atomRecoil';
 
 const PortfolioPage = () => {
-
-//   const [portfolio, setPortfolio] = useState(null);
-// const [isLoading, setIsLoading] = useState(true);
-// const { userId, username } = useRecoilValue(userAtom);
-// const user_id = userId;
-
-// const fetchPortfolio = async () => {
-//   try {
-//     const portfolioData = await getArtistById(user_id);
-//     setPortfolio(portfolioData);
-//     setIsLoading(false);
-//   } catch (error) {
-//     console.error('Error fetching portfolio:', error);
-//   }
-// };
-
-// useEffect(() => {
-//   const fetchData = async () => {
-//     fetchPortfolio();
-//   };
-
-//   fetchData();
-// }, []); 
-
-
   const [portfolio, setPortfolio] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const { userId, username } = useRecoilValue(userAtom);
+  const { userId } = useRecoilValue(userAtom);
   const [artist, setArtist] = useState([]);
   const [selectedArtistId, setSelectedArtistId] = useState(null);
 
   const fetchArtist = async () => {
     try {
-        const response = await getArtist();
-        setArtist(response.data);
+      const response = await getArtist();
+      setArtist(response.data);
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-};
+  };
 
-useEffect(() => {
-    const fetchData = async () => {
-        if (userId) {
-            await fetchArtist();
-        }
-    };
-
-    fetchData();
-}, [userId]);
-
-useEffect(() => {
-    const selectedArtist = artist.find(item => item.user_id === userId);
-    if (selectedArtist) {
-        setSelectedArtistId(selectedArtist.artist_id);
-    }
-}, [artist, userId]);
-
-console.log("artistId: " + selectedArtistId);
-  
-  
   useEffect(() => {
-    // const fetchArtist = async () =>{
-    //   try{
-    //     const artistData = await getArtist();
-    //     console.log("artistData "+artistData.data);
-    //     setArtist(artistData.data);
-    //     //console.log("artistId " + artistID)
-
-    //   }catch(error){
-    //     console.error("error" , error);
-    //   }
-    // }
-    const fetchPortfolio = async () => {
-      try {
-        const portfolioData = await getArtistById(selectedArtistId); 
-        setPortfolio(portfolioData);
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Error fetching portfolio:', error);
+    const fetchData = async () => {
+      if (userId) {
+        await fetchArtist();
       }
     };
 
-    // fetchArtist();
+    fetchData();
+  }, [userId]);
+
+  useEffect(() => {
+    const selectedArtist = artist.find(item => item.user_id === userId);
+    if (selectedArtist) {
+      setSelectedArtistId(selectedArtist.artist_id);
+    }
+  }, [artist, userId]);
+
+  console.log("artistId: " + selectedArtistId);
+
+  useEffect(() => {
+    const fetchPortfolio = async () => {
+      try {
+        if (selectedArtistId) {
+          const portfolioData = await getArtistById(selectedArtistId);
+          setPortfolio(portfolioData);
+        }
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching portfolio:', error);
+        setIsLoading(false);
+      }
+    };
+
     fetchPortfolio();
-  }, []);
-  console.log("portfolio after artistID" + portfolio)
+  }, [selectedArtistId]);
+
+  console.log("portfolio after artistID" + portfolio);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -107,4 +73,4 @@ console.log("artistId: " + selectedArtistId);
   );
 };
 
-export default PortfolioPage; 
+export default PortfolioPage;
